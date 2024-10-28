e9c0ea5d15fb003cbce04d4383d4fd3e9e9c577e
Vinod Kubsad <vinod.kubsad57@gmail.com>
2024-10-28 13:46:11 +0530

    logger changes

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,8 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
-import {
-  INVALID_EMAIL_MESSAGE,
-  PASSWORD_REQUIREMENTS_MESSAGE,
-} from 'src/shared/app.constants';
+import { INVALID_EMAIL_MESSAGE } from 'src/shared/app.constants';
 import { EMAIL_REGEX } from '../schemas/util';
 
 export class SignUpDto {
@@ -19,8 +16,8 @@ export class SignUpDto {
 
   @IsNotEmpty()
   @IsString()
-  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])()[0-9a-zA-Z!@#$&()`.+,/"-]{8,}$/, {
-    message: PASSWORD_REQUIREMENTS_MESSAGE,
-  })
+  // @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])()[0-9a-zA-Z!@#$&()`.+,/"-]{8,}$/, {
+  //   message: PASSWORD_REQUIREMENTS_MESSAGE,
+  // })
   readonly password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
-
-  await app.listen(3000);
+  app.enableCors();
+  const port = 3000;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
